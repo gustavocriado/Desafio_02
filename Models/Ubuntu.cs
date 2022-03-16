5483481b25c91fb06b24a95ad050e39d055f9d52
Gustavo Criado <gustavoliracriado30@gmail.com>
2022-03-16 20:34:25 -0300

    Segundo concluido

---
+++ Models/Ubuntu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_02.Models
+{
+    public class Ubuntu
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public decimal Altura { get; set; }
+        public double Peso { get; set; }
+        public bool EhDev { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Desafio_02.Models;
+using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Desafio_02
 {
@@ -10,36 +13,88 @@ namespace Desafio_02
             //Imprime na tela
             Console.WriteLine("UBUNTU 002 - STARTS...");
 
-            string nome1 = "Ubuntu1 Silva";
-            int idade1   = 33;
-            double peso1 = 88.50;
-            decimal altura1 = 1.65M;
-            bool EhDev1  = true;
+            #region Ubuntus
+            var ubuntu1 = new Ubuntu()
+            {
+                Nome = "Ubuntu1 Silva",
+                Idade = 33,
+                Peso = 88.50,
+                Altura = 2.10M,
+                EhDev = true
+            };
+            var ubuntu2 = new Ubuntu()
+            {
+                Nome = "Ubuntu2 Santos",
+                Idade = 26,
+                Peso = 88.50,
+                Altura = 1.65M,
+                EhDev = true
+            };
+            var ubuntu3 = new Ubuntu()
+            {
+                Nome = "Ubuntu3 Silva",
+                Idade = 85,
+                Peso = 66.50,
+                Altura = 1.40M,
+                EhDev = false
+            };
 
-            string nome2 = "Ubuntu2 Santos";
-            int idade2 = 26;
-            double peso2 = 84.50;
-            decimal altura2 = 2.10M;
-            bool EhDev2 = true;
+            List<Ubuntu> ubuntus = new List<Ubuntu>();
+            ubuntus.Add(ubuntu1);
+            ubuntus.Add(ubuntu2);
+            ubuntus.Add(ubuntu3);
+            #endregion
 
-            string nome3 = "Ubuntu3 Silva";
-            int idade3 = 85;
-            double peso3 = 66.50;
-            decimal altura3 = 1.40M;
-            bool EhDev3 = false;
-
-
+            #region T1
             //T1 - Somar a idade de todos Ubuntus
+            var idadeSomada = ubuntus.Select(x => x.Idade).Sum();
+            Console.WriteLine($"A idade dos ubuntus Somada é: {idadeSomada}\n");
+            Console.ReadKey();
+            #endregion
 
+            #region T2
             //T2 - Agrupar os nomes de todos Ubuntus
+            Console.WriteLine("Nome dos Ubuntus Agrupados:\n");
+            foreach (var ubuntu in ubuntus)
+            {
+                Console.WriteLine($"{ubuntu.Nome}");
+            }
+            Console.ReadKey();
+            Console.WriteLine("\n");
+            #endregion
 
-            //T3 - Calcular Média dos IMC de todos Ubuntus
+            #region T3
+            Console.WriteLine("Calculando Imc de todos os ubuntus\n");
+            var ubuntu1Imc = (decimal)ubuntu1.Peso / (ubuntu1.Altura * ubuntu1.Altura);
+            var ubuntu2Imc = (decimal)ubuntu2.Peso / (ubuntu2.Altura * ubuntu2.Altura);
+            var ubuntu3Imc = (decimal)ubuntu3.Peso / (ubuntu3.Altura * ubuntu3.Altura);
 
+            Console.WriteLine($"o Imc do primeiro Ubuntu é: {ubuntu1Imc.ToString("F2")}");
+            Console.WriteLine($"o Imc do segundo Ubuntu é: {ubuntu2Imc.ToString("F2")}");
+            Console.WriteLine($"o Imc terceiro Ubuntu é: {ubuntu3Imc.ToString("F2")}");
+            Console.ReadKey();
+            #endregion
+
+            #region T4
             //T4 - Contar quantos Ubuntus sao Devs
+            Console.WriteLine("\nQuantidade de Ubuntus Devs:");
+            Console.WriteLine($"{ubuntus.Where(x => x.EhDev == true).Count()}\n");
+            Console.ReadKey();
+            #endregion
+
+            #region T5
 
             //T5 - Exibir somente Ubuntu com nome Silva
+            Console.WriteLine("Ubuntus com Silva no Nome: \n");
 
+            var Silvas = ubuntus.Where(x => x.Nome.ToUpper().Contains("SILVA")).Select(x => x.Nome).ToList();
+            foreach (var Silvinha in Silvas)
+            {
+                Console.WriteLine($"{Silvinha}");
+            }
+            Console.ReadKey();
 
+            #endregion
 
             //Imprime na tela
             Console.WriteLine("UBUNTU 002 - ENDS...");
